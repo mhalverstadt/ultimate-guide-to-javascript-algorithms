660d0c98fb541dd6b727770554a447cc6a63645b
mhalverstadt <markhalverstadt@gmail.com>
2022-10-16 13:40:24 -0600

    vowelsCounter complete

--- Beginner/vowelsCounter/index-START.js
+++ Beginner/vowelsCounter/index-START.js
@@ -3,11 +3,26 @@ Given a string of text, return the number of vowels found within the text
 e.g vowelsCounter('anehizxcv') // will return 3
 */
 
+const vowels = ['a','e','i','o','u']
 
 function vowelsCounter(text) {
-    // Code goes here
+    let counter = 0
+    for(letter of text.toLowerCase()){
+        if (vowels.includes(letter)){
+            counter ++
+        }
+    }
+    return counter
 }
 
+function vowelsCounter(text){
+    let matchingInstances = text.match(/[aeiou]/gi);
+    if (matchingInstances){
+        return matchingInstances.length
+    }else{
+        return 0
+    }
+}
 
 
 module.exports = vowelsCounter;
